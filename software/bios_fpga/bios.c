64181be1638ff6702339516f098563862b71c8e0
Jiyong Park <jypark1257@gmail.com>
2025-08-18 23:21:44 +0900

    bios program for fpga

---
+++ software/bios_fpga/bios.c
@@ -0,0 +1,38 @@
+#include "ascii.h"
+#include "uart.h"
+#include "string.h"
+
+#define BUFFER_LEN 128
+
+typedef void (*entry_t)(void);
+
+int main(void)
+{
+    uart_init(50000000, 115200);
+
+    uwrite_int8s("\n\r");
+
+    for ( ; ; ) {
+        uwrite_int8s("RV_MPW> ");
+
+        int8_t buffer[BUFFER_LEN];
+        int8_t sel_pim[BUFFER_LEN];
+        int8_t size[BUFFER_LEN];
+        int8_t *input = read_token(buffer, BUFFER_LEN, " \x0d");
+
+        if (strcmp(input, "help") == 0) {
+            uwrite_int8s("Please ask jiyong, jiyong is the best!!");
+            uwrite_int8s("\n\r");
+        }  else {
+            uwrite_int8s("Unrecognized token: ");
+            uwrite_int8s(input);
+            uwrite_int8s("\n\r");
+        }
+    }
+
+    while (1);
+
+    return 0;
+}
+
+
